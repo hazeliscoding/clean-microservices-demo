c0a1a6d0ef938637461ec9affc003db1eef4c5d3
ducksoop <torresj1997@outlook.com>
2024-01-10 16:31:53 -0600

    feat: add get all brands handler

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllBrandsHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers;
+
+public class GetAllBrandsHandler(IBrandRepository brandRepository, IMapper mapper)
+    : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+{
+    private readonly IBrandRepository _brandRepository = brandRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+    {
+        var brandList = await brandRepository.GetAllBrands();
+        var brandResponseList = _mapper.Map<IList<BrandResponse>>(brandList);
+        return brandResponseList;
+    }
+}
