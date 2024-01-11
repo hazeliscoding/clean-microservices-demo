539fb383f3ab8d03c4bf9e400fab15055b4e3e6c
ducksoop <torresj1997@outlook.com>
2024-01-11 13:41:21 -0600

    feat: implement get command and delete product handlers

---
+++ Services/Catalog/Catalog.Application/Handlers/DeleteProductByIdHandler.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Application.Queries;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers;
+
+public class DeleteProductByIdHandler(IProductRepository productRepository) : IRequestHandler<DeleteProductByIdQuery, bool>
+{
+    public async Task<bool> Handle(DeleteProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await productRepository.DeleteProduct(request.Id);
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Queries/DeleteProductByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Catalog.Application.Queries;
+
+public class DeleteProductByIdQuery(string id) : IRequest<bool>
+{
+    public string Id { get; set; } = id;
+}
