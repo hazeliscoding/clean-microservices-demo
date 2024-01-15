e8b4a654a9ca53b6f774fa6ece27252f951e9d1a
ducksoop <torresj1997@outlook.com>
2024-01-15 10:39:01 -0600

    feat: creating basket entities

---
+++ Services/Basket/Basket.Core/Repositories/IBasketRepository.cs
@@ -0,0 +1,10 @@
+﻿using Basket.Core.Entities;
+
+namespace Basket.Core.Repositories;
+
+public interface IBasketRepository
+{
+    Task<ShoppingCart> GetBasket(string username);
+    Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart);
+    Task DeleteBasket(string username);
+}
