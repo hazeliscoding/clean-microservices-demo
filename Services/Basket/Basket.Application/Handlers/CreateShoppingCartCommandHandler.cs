c7f9553cfddad1e0a81ecbec0dd67af820e1f414
ducksoop <torresj1997@outlook.com>
2024-01-15 11:32:52 -0600

    feat: add create shopping cart command

---
+++ Services/Basket/Basket.Application/Handlers/CreateShoppingCartCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Basket.Application.Commands;
+using Basket.Application.Mappers;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
+using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Handlers;
+
+public class CreateShoppingCartCommandHandler(IBasketRepository basketRepository) : IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
+{
+    public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
+    {
+        // TODO: Call Discount service and apply coupons
+
+        var shoppingCart = await basketRepository.UpdateBasket(new ShoppingCart
+        {
+            UserName = request.UserName,
+            Items = request.Items
+        });
+
+        var shoppingCartResponse = BasketMapper.Mapper.Map<ShoppingCartResponse>(shoppingCart);
+        return shoppingCartResponse;
+    }
+}
