4ee85ba3e9ae9d3e1bd2bb2f96ab02502d95ca78
ducksoop <torresj1997@outlook.com>
2024-01-15 09:44:33 -0600

    feat: implement search filter and sorting

--- Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
+++ Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -2,18 +2,81 @@
 using Catalog.Core.Repositories;
 using Catalog.Core.Specs;
 using Catalog.Infrastructure.Data;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Catalog.Infrastructure.Repositories;
 
 public class ProductRepository(ICatalogContext context) : IProductRepository, IBrandRepository, ITypesRepository
 {
-    public async Task<IEnumerable<Product>> GetProducts(CatalogSpecParams catalogSpecParams)
+    public async Task<Pagination<Product>> GetProducts(CatalogSpecParams catalogSpecParams)
     {
-        return await context
-            .Products
-            .Find(p => true)
-            .ToListAsync();
+        var builder = Builders<Product>.Filter;
+        var filter = builder.Empty;
+        if (!string.IsNullOrEmpty(catalogSpecParams.Search))
+        {
+            var searchFilter = builder.Regex(x => x.Name, new BsonRegularExpression(catalogSpecParams.Search));
+            filter &= searchFilter;
+        }
+        if (!string.IsNullOrEmpty(catalogSpecParams.BrandId))
+        {
+            var brandFilter = builder.Eq(x => x.Brands.Id, catalogSpecParams.BrandId);
+            filter &= brandFilter;
+        }
+        if (!string.IsNullOrEmpty(catalogSpecParams.TypeId))
+        {
+            var typeFilter = builder.Eq(x => x.Types.Id, catalogSpecParams.TypeId);
+            filter &= typeFilter;
+        }
+        if (!string.IsNullOrEmpty(catalogSpecParams.Sort))
+        {
+            return new Pagination<Product>
+            {
+                PageIndex = catalogSpecParams.PageIndex,
+                Data = await DataFilter(catalogSpecParams, filter),
+                Count = await context
+                    .Products
+                    .CountDocumentsAsync(p => true) // TODO: Need to check while applying with UI
+            };
+        }
+
+        return new Pagination<Product>
+        {
+            PageSize = catalogSpecParams.PageSize,
+            PageIndex = catalogSpecParams.PageIndex,
+            Data = await context
+                .Products
+                .Find(filter)
+                .Sort((Builders<Product>.Sort.Ascending("Name")))
+                .Skip(catalogSpecParams.PageSize * (catalogSpecParams.PageIndex - 1))
+                .Limit(catalogSpecParams.PageSize)
+                .ToListAsync(),
+            Count = await context
+                .Products
+                .CountDocumentsAsync(p => true)
+        };
+    }
+
+    private async Task<IReadOnlyList<Product>> DataFilter(CatalogSpecParams catalogSpecParams, FilterDefinition<Product> filter)
+    {
+        return catalogSpecParams.Sort switch
+        {
+            "priceAsc" => await context.Products.Find(filter)
+                .Sort(Builders<Product>.Sort.Ascending("Price"))
+                .Skip(catalogSpecParams.PageSize * (catalogSpecParams.PageIndex - 1))
+                .Limit(catalogSpecParams.PageSize)
+                .ToListAsync(),
+            "priceDesc" => await context.Products.Find(filter)
+                .Sort(Builders<Product>.Sort.Descending("Price"))
+                .Skip(catalogSpecParams.PageSize * (catalogSpecParams.PageIndex - 1))
+                .Limit(catalogSpecParams.PageSize)
+                .ToListAsync(),
+            _ => await context.Products.Find(filter)
+                .Sort(Builders<Product>.Sort.Ascending("Name"))
+                .Skip(catalogSpecParams.PageSize * (catalogSpecParams.PageIndex - 1))
+                .Limit(catalogSpecParams.PageSize)
+                .ToListAsync()
+        };
     }
 
     public async Task<Product> GetProduct(string id)
