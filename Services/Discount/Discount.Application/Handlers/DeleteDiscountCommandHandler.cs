bf7ecc3e308b6f41507b75c44c71009127a67885
ducksoop <torresj1997@outlook.com>
2024-02-22 16:12:57 -0600

    feat: 🎸 application logic for discount service implemented

---
+++ Services/Discount/Discount.Application/Handlers/DeleteDiscountCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Discount.Application.Commands;
+using Discount.Core.Repositories;
+using MediatR;
+
+namespace Discount.Application.Handlers;
+
+public class DeleteDiscountCommandHandler(IDiscountRepository discountRepository) : IRequestHandler<DeleteDiscountCommand, bool>
+{
+    public async Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+    {
+        var deleted = await discountRepository.DeleteDiscount(request.ProductName);
+        return deleted;
+    }
+}
---
+++ Services/Discount/Discount.Application/Handlers/UpdateDiscountCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Discount.Application.Commands;
+using Discount.Application.Mapper;
+using Discount.Core.Entities;
+using Discount.Core.Repositories;
+using Discount.Grpc.Protos;
+using MediatR;
+
+namespace Discount.Application.Handlers;
+
+public class UpdateDiscountCommandHandler(IDiscountRepository discountRepository) : IRequestHandler<UpdateDiscountCommand, CouponModel>
+{
+    public async Task<CouponModel> Handle(UpdateDiscountCommand request, CancellationToken cancellationToken)
+    {
+        var coupon = DiscountMapper.Mapper.Map<Coupon>(request);
+        await discountRepository.UpdateDiscount(coupon);
+        return DiscountMapper.Mapper.Map<CouponModel>(coupon);
+    }
+}
