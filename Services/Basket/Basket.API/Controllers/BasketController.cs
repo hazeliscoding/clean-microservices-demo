6f04ecd909779f20ffd2541c5c80d41e201f4dde
ducksoop <torresj1997@outlook.com>
2024-02-28 21:30:25 -0600

    feat: 🎸 calling discount service from basket api

--- Services/Basket/Basket.API/Controllers/BasketController.cs
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using Basket.Application.Commands;
+using Basket.Application.GrpcService;
 using Basket.Application.Queries;
 using Basket.Application.Responses;
 using MediatR;
@@ -7,22 +8,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.API.Controllers;
 
-public class BasketController : ApiController
+public class BasketController(IMediator mediator, DiscountGrpcService discountGrpcService)
+    : ApiController
 {
-    private readonly IMediator _mediator;
-    
-    public BasketController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     [HttpGet]
     [Route("[action]/{userName}", Name = "GetBasketByUserName")]
     [ProducesResponseType(typeof(ShoppingCartResponse), (int) HttpStatusCode.OK)]
     public async Task<ActionResult<ShoppingCartResponse>> GetBasket(string userName)
     {
         var query = new GetBasketByUserNameQuery(userName);
-        var basket = await _mediator.Send(query);
+        var basket = await mediator.Send(query);
         return Ok(basket);
     }
 
@@ -30,7 +25,13 @@ public class BasketController : ApiController
     [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
     public async Task<ActionResult<ShoppingCartResponse>> UpdateBasket([FromBody] CreateShoppingCartCommand createShoppingCartCommand)
     {
-        var basket = await _mediator.Send(createShoppingCartCommand);
+        foreach (var item in createShoppingCartCommand.Items)
+        {
+            var coupon = await discountGrpcService.GetDiscount(item.ProductName);
+            item.Price -= coupon.Amount;
+        }
+
+        var basket = await mediator.Send(createShoppingCartCommand);
         return Ok(basket);
     }
 
@@ -40,6 +41,6 @@ public class BasketController : ApiController
     public async Task<ActionResult<ShoppingCartResponse>> DeleteBasket(string userName)
     {
         var command = new DeleteBasketByUserNameCommand(userName);
-        return Ok(await _mediator.Send(command));
+        return Ok(await mediator.Send(command));
     }
 }
