10246ada83ebabb5945ab117a916348bb7fccf18
hazeliscoding <duck_soop@outlook.com>
2024-03-15 09:45:08 -0500

    feat: 🎸 creating OrderContextFactory and seeding data

---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data;
+
+public class OrderContextSeed
+{
+    public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+    {
+        if (!orderContext.Orders.Any())
+        {
+            orderContext.Orders.AddRange(GetOrders());
+            await orderContext.SaveChangesAsync();
+            logger.LogInformation($"Ordering Database seeded: {nameof(OrderContext)}");
+        }
+    }
+
+    private static IEnumerable<Order> GetOrders()
+    {
+        return new List<Order>
+        {
+            new()
+            {
+                UserName = "JohnDoe",
+                TotalPrice = 100.00m,
+                FirstName = "John",
+                LastName = "Doe",
+                EmailAddress = "johndoe@example.com",
+                AddressLine = "123 Main St",
+                Country = "USA",
+                State = "California",
+                ZipCode = "12345",
+                CardName = "John Doe",
+                CardNumber = "1234567890123456",
+                Expiration = "12/23",
+                Cvv = "123",
+                PaymentMethod = 1
+            },
+            new Order
+            {
+                UserName = "JaneSmith",
+                TotalPrice = 200.00m,
+                FirstName = "Jane",
+                LastName = "Smith",
+                EmailAddress = "janesmith@example.com",
+                AddressLine = "456 Elm St",
+                Country = "USA",
+                State = "New York",
+                ZipCode = "67890",
+                CardName = "Jane Smith",
+                CardNumber = "9876543210987654",
+                Expiration = "11/24",
+                Cvv = "456",
+                PaymentMethod = 2
+            }
+        };
+    }
+}
