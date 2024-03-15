79deeee9225562ea20224d114195068ee0eb618f
hazeliscoding <duck_soop@outlook.com>
2024-03-15 09:55:39 -0500

    feat: 🎸 order repository implementation

---
+++ Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure.Repositories;
+
+public class OrderRepository(OrderContext dbContext) : RepositoryBase<Order>(dbContext)
+{
+    private readonly OrderContext _dbContext = dbContext;
+
+    public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+    {
+        var orderList = await _dbContext.Orders
+            .Where(o => o.UserName == userName)
+            .ToListAsync();
+        return orderList;
+    }
+}
