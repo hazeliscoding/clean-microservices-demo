a629cadcdebc53be3fdadaa0c9fb5ac2343518d0
hazeliscoding <duck_soop@outlook.com>
2024-03-18 08:55:50 -0500

    feat: 🎸 delete order command handler

---
+++ Services/Ordering/Ordering.Application/Commands/DeleteOrderCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Commands;
+
+public class DeleteOrderCommand(int id) : IRequest<Unit>
+{
+    public int Id { get; set; } = id;
+}
---
+++ Services/Ordering/Ordering.Application/Handlers/DeleteCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Commands;
+using Ordering.Application.Extensions;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Handlers;
+
+public class DeleteCommandHandler(
+    IOrderRepository orderRepository,
+    ILogger<DeleteCommandHandler> logger) : IRequestHandler<DeleteOrderCommand, Unit>
+{
+    public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+    {
+        var orderToDelete = await orderRepository.GetByIdAsync(request.Id);
+        if (orderToDelete == null)
+        {
+            logger.LogError("Order not found");
+            throw new OrderNotFoundException(nameof(Order), request.Id);
+        }
+        
+        await orderRepository.DeleteAsync(orderToDelete);
+        
+        logger.LogInformation($"Order {orderToDelete.Id} is successfully deleted.");
+        return Unit.Value;
+    }
+}
