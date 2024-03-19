dc2dbca069ea27d2f5b228ae6146ebc3aec99ed2
hazeliscoding <duck_soop@outlook.com>
2024-03-19 09:03:57 -0500

    feat: 🎸 add order.api controllers

--- Services/Ordering/Ordering.Application/Commands/DeleteOrderCommand.cs
+++ Services/Ordering/Ordering.Application/Commands/DeleteOrderCommand.cs
@@ -2,7 +2,7 @@
 
 namespace Ordering.Application.Commands;
 
-public class DeleteOrderCommand(int id) : IRequest<Unit>
+public class DeleteOrderCommand : IRequest<Unit>
 {
-    public int Id { get; set; } = id;
+    public int Id { get; set; }
 }
