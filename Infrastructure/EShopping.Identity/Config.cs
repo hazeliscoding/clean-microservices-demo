585f50653ae07574633256099c53ed301f2f0402
hazeliscoding <duck_soop@outlook.com>
2024-04-19 14:37:54 -0500

    feat: 🎸 machine to machine flow for catalog api

--- Infrastructure/EShopping.Identity/Config.cs
+++ Infrastructure/EShopping.Identity/Config.cs
@@ -19,46 +19,31 @@ namespace EShopping.Identity
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
+                new ApiScope("catalogapi")
             };
 
         public static IEnumerable<ApiResource> ApiResources =>
             new ApiResource[]
             {
                 // List of Microservices can go here.
+                new ApiResource("Catalog", "Catalog.API")
+                {
+                    Scopes = {"catalogapi"}
+                }
             };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-                // m2m client credentials flow client
+                // m2m flow
                 new Client
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
+                    ClientName = "Catalog API Client",
+                    ClientId = "CatalogAPIClient",
+                    ClientSecrets = {new Secret("5c6eb3b4-61a7-4668-ac57-2b4591ec26d2".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-                    
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
+                    AllowedScopes = {"catalogapi"}
+                }
             };
     }
 }
